722c92d64798fc7653ac2e6e3b72e5d82013bdca
Priyamvada554 <priyambada.pcm@gmail.com>
2026-05-01 11:37:49 +0530

    Remove trailing '/api' from API_BASE_URL

--- raahi-fr/src/util/api.js
+++ raahi-fr/src/util/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL + '/api';
+export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const postBlog = async (formData) => {
     try {
