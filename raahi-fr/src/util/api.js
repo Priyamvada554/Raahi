8a534f344153a0915f5c56fcb4ee90f885c81c64
Priyamvada554 <priyambada.pcm@gmail.com>
2026-05-01 11:05:57 +0530

    Update API_BASE_URL and adjust API endpoints

--- raahi-fr/src/util/api.js
+++ raahi-fr/src/util/api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL;
-
-
-
+export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL + '/api';
 
 export const postBlog = async (formData) => {
     try {
@@ -22,10 +19,6 @@ export const postBlog = async (formData) => {
     }
 };
 
-
-
-
-
 export const fetchLatestBlogs = async () => {
     try {
         const res = await axios.get(`${API_BASE_URL}/blogs/latest`);
@@ -35,9 +28,6 @@ export const fetchLatestBlogs = async () => {
         return [];
     }
 };
-
-
-
 
 export const getBlogsByUser = async (userId) => {
     try {
@@ -49,30 +39,22 @@ export const getBlogsByUser = async (userId) => {
     }
 };
 
-
-
 export const searchBlogs = async (country, state) => {
-
     try {
         const params = {};
         if (country) params.country = country;
-        if (state) params.state = state
-
-        const res = await axios.get(`${import.meta.env.VITE_API_URL}/blogs/search`, { params });
+        if (state) params.state = state;
+        const res = await axios.get(`${API_BASE_URL}/blogs/search`, { params });
         return res.data.blogs;
-    }
-    catch (err) {
+    } catch (err) {
         console.error("Search failed", err);
         return [];
-
     }
-}
-
+};
 
 export const blogDetails = async (id) => {
-
     try {
-        const res = await axios.get(`${import.meta.env.VITE_API_URL}/blogs/${id}`);
+        const res = await axios.get(`${API_BASE_URL}/blogs/${id}`);
         return res.data.blog;
     } catch (error) {
         console.error("Failed to fetch blog details:", error);
